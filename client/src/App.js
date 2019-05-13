885843d7e2fe4dd6d89879222de2065560c93ed3
Robert <robert.wagner@pepper.com>
2019-05-13 17:04:15 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Fib from './Fib';
 
 class App extends Component {
@@ -10,8 +9,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version!</h1>
+            <h1 className="App-title">Fibonnaci Calculator</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
