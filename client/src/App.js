fa8b6f5942de36498318270b904e36ffe7b8009d
Robert <robert.wagner@pepper.com>
2019-05-13 16:07:47 +0200

    updated client

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version KUBERNETES!</h1>
+            <h1 className="App-title">Fib Calculator version!</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
