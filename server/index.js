0efc7100bd746065147132afd5ae00acbacc8646
Robert <robert.wagner@pepper.com>
2019-05-13 17:16:08 +0200

    updated

--- server/index.js
+++ server/index.js
@@ -35,19 +35,19 @@ const redisPublisher = redisClient.duplicate();
 
 
 // Express Routes
-app.get('/values/all', async (req, res) => {
+app.get('/api/values/all', async (req, res) => {
   const values = await dbClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/api/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.post('/api/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
